43607f7dd1484e0f3a0f79db6f6f484090498828
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-09 18:15:16 +0600

    Multi Auth- Create Middleware for Multi Auth

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        if($request->user()->role === $role){
+            return $next($request);
+        }
+
+        return to_route('dashboard');
+    }
+}

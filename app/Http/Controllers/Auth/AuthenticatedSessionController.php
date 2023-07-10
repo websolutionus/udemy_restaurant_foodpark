effec7877eba2fc123596f4c43de36f59458c937
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-10 10:18:57 +0600

    Multi Auth- Redirect to Dashboard Depending on Role

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,6 +29,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if($request->user()->role === 'admin'){
+            return redirect()->intended(RouteServiceProvider::ADMIN);
+        }
+        
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 

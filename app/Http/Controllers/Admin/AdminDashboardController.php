995f3372f2cbc8cf52d59ec19d343a53e1546ee2
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-10 13:47:09 +0600

    Admin Setup - Admin Panel Mastering (Part - 1)

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 class AdminDashboardController extends Controller
 {
     function index() : View {
-        return view('admin.dashboard.index');
+        // return view('admin.dashboard.index');
+        return view('admin.layouts.master');
+
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){

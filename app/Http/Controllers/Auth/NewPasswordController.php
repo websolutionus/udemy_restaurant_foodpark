f514e4eda640daa47b3328d6e9c989d7fe62b530
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-11 13:23:53 +0600

    Frontend - Reset Password Page Mastering

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -50,6 +50,7 @@ class NewPasswordController extends Controller
             }
         );
 
+        toastr('Password reset successfully!', 'success');
         // If the password was successfully reset, we will redirect the user back to
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -36,4 +36,89 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+@extends('frontend.layouts.master')
+
+@section('content')
+    <!--=============================
+        BREADCRUMB START
+    ==============================-->
+    <section class="fp__breadcrumb" style="background: url({{ asset('frontend/images/counter_bg.jpg') }});">
+        <div class="fp__breadcrumb_overlay">
+            <div class="container">
+                <div class="fp__breadcrumb_text">
+                    <h1>reset password</h1>
+                    <ul>
+                        <li><a href="javascript:;">home</a></li>
+                        <li><a href="#">reset password</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=============================
+        BREADCRUMB END
+    ==============================-->
+
+
+    <!--=========================
+        FORGOT PASSWORD START
+    ==========================-->
+    <section class="fp__signin" style="background: url({{ asset('frontend/images/login_bg.jpg') }});">
+        <div class="fp__signin_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+            <div class="container">
+                <div class="row wow fadeInUp" data-wow-duration="1s">
+                    <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                        <div class="fp__login_area">
+                            <h2>Welcome back!</h2>
+                            <p>forgot password</p>
+                            <form method="POST" action="{{ route('password.store') }}">
+                                @csrf
+                                <div class="row">
+                                                        <!-- Password Reset Token -->
+                                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>email</label>
+                                            <input type="email" name="email" value="{{ old('email', $request->email) }}" placeholder="Email">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>password</label>
+                                            <input type="password" name="password"  placeholder="Password">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>confirm password</label>
+                                            <input  type="password" name="password_confirmation" placeholder="Password">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <button type="submit" class="common_btn">reset password</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <p class="create_account d-flex justify-content-between">
+                                <a href="{{ route('login') }}">login</a>
+                                <a href="{{ route('register') }}">Create Account</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=========================
+        FORGOT PASSWORD END
+    ==========================-->
+@endsection

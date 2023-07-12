25c2e819c1f79b38689b654e12a91d0a4284d5ac
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-12 15:42:48 +0600

    Admin - Admin Logout Feature

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -27,9 +27,16 @@
                     <i class="fas fa-cog"></i> Settings
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item has-icon text-danger">
-                    <i class="fas fa-sign-out-alt"></i> Logout
-                </a>
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <a href="#" onclick="event.preventDefault();
+                    this.closest('form').submit();" class="dropdown-item has-icon text-danger">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                </form>
+
             </div>
         </li>
     </ul>

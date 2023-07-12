5a8d07d72801d07f3beb151a23458be800729849
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-12 15:12:28 +0600

    Admin - Admin Avatar  Update Feature (Part - 4)

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ProfilePasswordUpdateRequest;
 use App\Http\Requests\Admin\ProfileUpdateRequest;
+use App\Traits\FileUploadTrait;
 use Auth;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
@@ -12,16 +13,21 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    use FileUploadTrait;
+
     function index() : View {
         return view('admin.profile.index');
     }
 
     function updateProfile(ProfileUpdateRequest $request) : RedirectResponse {
-        
+
         $user = Auth::user();
+
+        $imagePath = $this->uploadImage($request, 'avatar');
 
         $user->name = $request->name;
         $user->email = $request->email;
+        $user->avatar = isset($imagePath) ? $imagePath : $user->avatar;
         $user->save();
 
         toastr('Updated Successfully!', 'success');
--- app/Traits/FileUploadTrait.php
+++ app/Traits/FileUploadTrait.php
@@ -2,21 +2,24 @@
 
 namespace App\Traits;
 
-use Request;
+use Illuminate\Http\Request;
 
 trait FileUploadTrait {
 
 
-    function updateImage(Request $request, $inputName, $path = "/uploads") {
+    function uploadImage(Request $request, $inputName, $path = "/uploads") {
+
         if($request->hasFile($inputName)){
             $image = $request->{$inputName};
             $ext = $image->getClientOriginalExtension();
             $imageName = 'media_'.uniqid().'.'.$ext;
 
-            $image->move(public_path($path));
+            $image->move(public_path($path), $imageName);
 
             return $path.'/'.$imageName;
         }
+
+        return NULL;
     }
-    
+
 }

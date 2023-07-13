5ff24dfcf1e140df3410b9a5d70ed38c4cc70904
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-13 12:21:13 +0600

    Frontend - Fixing the Multi Auth Login Issue

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 /** Admin Auth Routes */
-Route::get('admin/login', [AdminAuthController::class, 'index'])->name('admin.login');
-Route::get('admin/forget-password', [AdminAuthController::class, 'forgetPassword'])->name('admin.forget-password');
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('admin/login', [AdminAuthController::class, 'index'])->name('admin.login');
+    Route::get('admin/forget-password', [AdminAuthController::class, 'forgetPassword'])->name('admin.forget-password');
+});
 
 Route::group(['middleware' => 'auth'], function(){
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');

24ed5d7693ed2f7c2ab08d3e37980a98207c6c53
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-15 10:12:25 +0600

    Home Slider- Update Slider Feature (Part - 2)

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -63,9 +63,10 @@ class SliderController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
-        //
+        $slider = Slider::findOrFail($id);
+        return view('admin.slider.edit', compact('slider'));
     }
 
     /**
---
+++ resources/views/admin/slider/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('admin.layouts.master')
+
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>Slider</h1>
+    </div>
+
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Update Slider</h4>
+
+        </div>
+        <div class="card-body">
+            <form action="{{ route('admin.slider.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label>Image</label>
+                    <div id="image-preview" class="image-preview">
+                        <label for="image-upload" id="image-label">Choose File</label>
+                        <input type="file" name="image" id="image-upload" />
+                      </div>
+                </div>
+
+                <div class="form-group">
+                    <label>Offer</label>
+                    <input type="text" name="offer" class="form-control" value="{{ $slider->offer }}">
+                </div>
+                <div class="form-group">
+                    <label>Title</label>
+                    <input type="text" name="title" class="form-control" value="{{ $slider->title }}">
+                </div>
+                <div class="form-group">
+                    <label>Sub Title</label>
+                    <input type="text" name="sub_title" class="form-control" value="{{ $slider->sub_title }}">
+                </div>
+                <div class="form-group">
+                    <label>Short Description</label>
+                    <textarea name="short_description" class="form-control">{{ $slider->short_description }}</textarea>
+                </div>
+                <div class="form-group">
+                    <label>Button Link</label>
+                    <input type="text" name="button_link" class="form-control" value="{{ $slider->button_link }}">
+                </div>
+                <div class="form-group">
+                    <label>Status</label>
+                    <select name="status" class="form-control" id="">
+                        <option @selected($slider->status === 1) value="1">Active</option>
+                        <option @selected($slider->status === 0) value="0">Inactive</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Update</button>
+            </form>
+        </div>
+    </div>
+</section>
+@endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function(){
+            $('.image-preview').css({
+                'background-image': 'url({{ asset($slider->image) }})',
+                'background-size': 'cover',
+                'background-position': 'center center'
+            })
+        })
+    </script>
+@endpush

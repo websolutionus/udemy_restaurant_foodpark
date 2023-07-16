3a6b2e5c0ae5a2dcca788af91e5e979e64b47564
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-07-16 10:21:20 +0600

    Why choose us - Creating Necessary Files and Designs (Part - 2)

--- resources/views/admin/why-choose-us/index.blade.php
+++ resources/views/admin/why-choose-us/index.blade.php
@@ -6,6 +6,38 @@
             <h1>Why Choose Us</h1>
         </div>
 
+        <div class="card">
+            <div class="card-body">
+                <div id="accordion">
+                    <div class="accordion">
+                        <div class="accordion-header collapsed bg-primary text-light p-3 " role="button"
+                            data-toggle="collapse" data-target="#panel-body-1" aria-expanded="false">
+                            <h4>Why Choose Us Section Titles..</h4>
+                        </div>
+                        <div class="accordion-body collapse" id="panel-body-1" data-parent="#accordion" style="">
+                            <div class="form-group">
+                                <label for="">Top Title</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Main Title</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Sub Title</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="section">
+
         <div class="card card-primary">
             <div class="card-header">
                 <h4>All Items</h4>

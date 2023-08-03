0a5cb4a0e7a1065e33a813f66b29623380251473
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-03 18:11:28 +0600

    Showing Products - Show Dynamic Product in Food Menu Section (Part - 2)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
+
+    function category() : BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/frontend/home/components/menu-item.blade.php
+++ resources/views/frontend/home/components/menu-item.blade.php
@@ -27,33 +27,50 @@
         </div>
 
         <div class="row grid">
+            @foreach ($categories as $category)
+                @php
+                    $products = \App\Models\Product::where(['show_at_home' => 1, 'status' => 1, 'category_id' => $category->id])
+                        ->orderBy('id', 'DESC')
+                        ->take(8)
+                        ->get();
+                @endphp
 
-            <div class="col-xl-3 col-sm-6 col-lg-4  chicken dresserts wow fadeInUp" data-wow-duration="1s">
-                <div class="fp__menu_item">
-                    <div class="fp__menu_item_img">
-                        <img src="images/menu2_img_2.jpg" alt="menu" class="img-fluid w-100">
-                        <a class="category" href="#">chicken</a>
-                    </div>
-                    <div class="fp__menu_item_text">
-                        <p class="rating">
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star"></i>
-                            <i class="fas fa-star-half-alt"></i>
-                            <i class="far fa-star"></i>
-                            <span>145</span>
-                        </p>
-                        <a class="title" href="menu_details.html">chicken Masala</a>
-                        <h5 class="price">$80.00 <del>90.00</del></h5>
-                        <ul class="d-flex flex-wrap justify-content-center">
-                            <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
-                                        class="fas fa-shopping-basket"></i></a></li>
-                            <li><a href="#"><i class="fal fa-heart"></i></a></li>
-                            <li><a href="#"><i class="far fa-eye"></i></a></li>
-                        </ul>
+                @foreach ($products as $product)
+                <div class="col-xl-3 col-sm-6 col-lg-4 {{ $category->slug }} wow fadeInUp" data-wow-duration="1s">
+                    <div class="fp__menu_item">
+                        <div class="fp__menu_item_img">
+                            <img src="{{ asset($product->thumb_image) }}" alt="{{ $product->name }}" class="img-fluid w-100">
+                            <a class="category" href="#">{{ @$product->category->name }}</a>
+                        </div>
+                        <div class="fp__menu_item_text">
+                            <p class="rating">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star-half-alt"></i>
+                                <i class="far fa-star"></i>
+                                <span>145</span>
+                            </p>
+                            <a class="title" href="menu_details.html">{{ $product->name }}</a>
+                            <h5 class="price">
+                                @if ($product->offer_price > 0)
+                                ${{ $product->offer_price }}
+                                <del>${{ $product->price }}</del>
+                                @else
+                                ${{ $product->price }}
+                                @endif
+                            </h5>
+                            <ul class="d-flex flex-wrap justify-content-center">
+                                <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
+                                            class="fas fa-shopping-basket"></i></a></li>
+                                <li><a href="#"><i class="fal fa-heart"></i></a></li>
+                                <li><a href="#"><i class="far fa-eye"></i></a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
+                @endforeach
+            @endforeach
 
         </div>
     </div>

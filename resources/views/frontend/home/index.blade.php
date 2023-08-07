2df197ae21e58e4568565924e91e75cf82381d01
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-07 13:22:36 +0600

    Cart Feature -  Working with Quantity Increment - Decrement

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -22,7 +22,7 @@
     <!--=============================
             OFFER ITEM START
         ==============================-->
-    @include('frontend.home.components.offer-item')
+    {{-- @include('frontend.home.components.offer-item') --}}
 
     <!-- CART POPUT START -->
     @include('frontend.home.components.cart-popup')
--- resources/views/frontend/layouts/ajax-files/product-popup-modal.blade.php
+++ resources/views/frontend/layouts/ajax-files/product-popup-modal.blade.php
@@ -61,9 +61,9 @@
     <h5>select quentity</h5>
     <div class="quentity_btn_area d-flex flex-wrapa align-items-center">
         <div class="quentity_btn">
-            <button class="btn btn-danger"><i class="fal fa-minus"></i></button>
-            <input type="text" placeholder="1">
-            <button class="btn btn-success"><i class="fal fa-plus"></i></button>
+            <button class="btn btn-danger decrement"><i class="fal fa-minus"></i></button>
+            <input type="text" id="quantity" placeholder="1" value="1" readonly>
+            <button class="btn btn-success increment"><i class="fal fa-plus"></i></button>
         </div>
         @if ($product->offer_price > 0)
         <h3 id="total_price">{{ currencyPosition($product->offer_price) }}</h3>
@@ -90,11 +90,33 @@
             updateTotalPrice();
         });
 
+        // Event handlers for increment and decrement buttons
+        $('.increment').on('click', function(e){
+            e.preventDefault()
+
+            let quantity = $('#quantity');
+            let currentQuantity = parseFloat(quantity.val());
+            quantity.val(currentQuantity + 1);
+            updateTotalPrice()
+        })
+
+        $('.decrement').on('click', function(e){
+            e.preventDefault()
+
+            let quantity = $('#quantity');
+            let currentQuantity = parseFloat(quantity.val());
+            if(currentQuantity > 1){
+                quantity.val(currentQuantity - 1);
+                updateTotalPrice()
+            }
+        })
+
         // Function to update the total price base on seelected options
         function updateTotalPrice() {
             let basePrice = parseFloat($('input[name="base_price"]').val());
             let selectedSizePrice = 0;
             let selectedOptionsPrice = 0;
+            let quantity = parseFloat($('#quantity').val());
 
             // Calculate the selected size price
             let selectedSize = $('input[name="product_size"]:checked');
@@ -109,7 +131,7 @@
             })
 
             // Calculate the total price
-            let totalPrice = basePrice + selectedSizePrice + selectedOptionsPrice;
+            let totalPrice = (basePrice + selectedSizePrice + selectedOptionsPrice) * quantity;
             $('#total_price').text("{{ config('settings.site_currency_icon') }}" + totalPrice);
 
         }

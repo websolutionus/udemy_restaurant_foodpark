0c7a9f848d490ce73c2bd212f918b6d03af4e1e7
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-07 15:39:41 +0600

    Cart Feature -  Adding Loader on Ajax Request

--- resources/views/frontend/layouts/global-scripts.blade.php
+++ resources/views/frontend/layouts/global-scripts.blade.php
@@ -5,12 +5,18 @@ function loadProductModal(productId){
     $.ajax({
         method: 'GET',
         url: '{{ route("load-product-modal", ":productId") }}'.replace(':productId', productId),
+        beforeSend: function(){
+            $('.overlay').addClass('active');
+        },
         success: function(response){
             $(".load_product_modal_body").html(response);
             $('#cartModal').modal('show');
         },
         error: function(xhr, status, error){
             console.error(error);
+        },
+        complete: function(){
+            $('.overlay').removeClass('active');
         }
     })
 }
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -21,125 +21,132 @@
 
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css') }}">
+
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
 </head>
 
 <body>
+    <div class="overlay-container">
+        <div class="overlay">
+            <span class="loader"></span>
+        </div>
+    </div>
 
     <!--=============================
         TOPBAR START
     ==============================-->
-    <section class="fp__topbar">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-6 col-md-8">
-                    <ul class="fp__topbar_info d-flex flex-wrap">
-                        <li><a href="mailto:example@gmail.com"><i class="fas fa-envelope"></i> Unifood@gmail.com</a>
-                        </li>
-                        <li><a href="callto:123456789"><i class="fas fa-phone-alt"></i> +96487452145214</a></li>
-                    </ul>
-                </div>
-                <div class="col-xl-6 col-md-4 d-none d-md-block">
-                    <ul class="topbar_icon d-flex flex-wrap">
-                        <li><a href="#"><i class="fab fa-facebook-f"></i></a> </li>
-                        <li><a href="#"><i class="fab fa-twitter"></i></a> </li>
-                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a> </li>
-                        <li><a href="#"><i class="fab fa-behance"></i></a> </li>
-                    </ul>
+        <section class="fp__topbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-md-8">
+                        <ul class="fp__topbar_info d-flex flex-wrap">
+                            <li><a href="mailto:example@gmail.com"><i class="fas fa-envelope"></i> Unifood@gmail.com</a>
+                            </li>
+                            <li><a href="callto:123456789"><i class="fas fa-phone-alt"></i> +96487452145214</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xl-6 col-md-4 d-none d-md-block">
+                        <ul class="topbar_icon d-flex flex-wrap">
+                            <li><a href="#"><i class="fab fa-facebook-f"></i></a> </li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a> </li>
+                            <li><a href="#"><i class="fab fa-linkedin-in"></i></a> </li>
+                            <li><a href="#"><i class="fab fa-behance"></i></a> </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <!--=============================
+        </section>
+        <!--=============================
         TOPBAR END
     ==============================-->
 
 
-    <!--=============================
+        <!--=============================
         MENU START
     ==============================-->
-    @include('frontend.layouts.menu')
-    <!--=============================
+        @include('frontend.layouts.menu')
+        <!--=============================
         MENU END
     ==============================-->
 
 
-    @yield('content')
+        @yield('content')
 
 
-    <!--=============================
+        <!--=============================
         FOOTER START
     ==============================-->
-    @include('frontend.layouts.footer')
-    <!--=============================
+        @include('frontend.layouts.footer')
+        <!--=============================
         FOOTER END
     ==============================-->
 
 
-    <!--=============================
+        <!--=============================
         SCROLL BUTTON START
     ==============================-->
-    <div class="fp__scroll_btn">
-        go to top
-    </div>
-    <!--=============================
+        <div class="fp__scroll_btn">
+            go to top
+        </div>
+        <!--=============================
         SCROLL BUTTON END
     ==============================-->
 
 
-    <!--jquery library js-->
-    <script src="{{ asset('frontend/js/jquery-3.6.0.min.js') }}"></script>
-    <!--bootstrap js-->
-    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
-    <!--font-awesome js-->
-    <script src="{{ asset('frontend/js/Font-Awesome.js') }}"></script>
-    <!-- slick slider -->
-    <script src="{{ asset('frontend/js/slick.min.js') }}"></script>
-    <!-- isotop js -->
-    <script src="{{ asset('frontend/js/isotope.pkgd.min.js') }}"></script>
-    <!-- simplyCountdownjs -->
-    <script src="{{ asset('frontend/js/simplyCountdown.js') }}"></script>
-    <!-- counter up js -->
-    <script src="{{ asset('frontend/js/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('frontend/js/jquery.countup.min.js') }}"></script>
-    <!-- nice select js -->
-    <script src="{{ asset('frontend/js/jquery.nice-select.min.js') }}"></script>
-    <!-- venobox js -->
-    <script src="{{ asset('frontend/js/venobox.min.js') }}"></script>
-    <!-- sticky sidebar js -->
-    <script src="{{ asset('frontend/js/sticky_sidebar.js') }}"></script>
-    <!-- wow js -->
-    <script src="{{ asset('frontend/js/wow.min.js') }}"></script>
-    <!-- ex zoom js -->
-    <script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
+        <!--jquery library js-->
+        <script src="{{ asset('frontend/js/jquery-3.6.0.min.js') }}"></script>
+        <!--bootstrap js-->
+        <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+        <!--font-awesome js-->
+        <script src="{{ asset('frontend/js/Font-Awesome.js') }}"></script>
+        <!-- slick slider -->
+        <script src="{{ asset('frontend/js/slick.min.js') }}"></script>
+        <!-- isotop js -->
+        <script src="{{ asset('frontend/js/isotope.pkgd.min.js') }}"></script>
+        <!-- simplyCountdownjs -->
+        <script src="{{ asset('frontend/js/simplyCountdown.js') }}"></script>
+        <!-- counter up js -->
+        <script src="{{ asset('frontend/js/jquery.waypoints.min.js') }}"></script>
+        <script src="{{ asset('frontend/js/jquery.countup.min.js') }}"></script>
+        <!-- nice select js -->
+        <script src="{{ asset('frontend/js/jquery.nice-select.min.js') }}"></script>
+        <!-- venobox js -->
+        <script src="{{ asset('frontend/js/venobox.min.js') }}"></script>
+        <!-- sticky sidebar js -->
+        <script src="{{ asset('frontend/js/sticky_sidebar.js') }}"></script>
+        <!-- wow js -->
+        <script src="{{ asset('frontend/js/wow.min.js') }}"></script>
+        <!-- ex zoom js -->
+        <script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
 
-    <script src="{{ asset('frontend/js/toastr.min.js') }}"></script>
+        <script src="{{ asset('frontend/js/toastr.min.js') }}"></script>
 
-    <!--main/custom js-->
-    <script src="{{ asset('frontend/js/main.js') }}"></script>
+        <!--main/custom js-->
+        <script src="{{ asset('frontend/js/main.js') }}"></script>
 
-    <!-- show dynamic validation message-->
-    <script>
-        toastr.options.progressBar = true;
+        <!-- show dynamic validation message-->
+        <script>
+            toastr.options.progressBar = true;
 
-        @if ($errors->any())
-            @foreach ($errors->all() as $error)
-                toastr.error("{{ $error }}")
-            @endforeach
-        @endif
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)
+                    toastr.error("{{ $error }}")
+                @endforeach
+            @endif
 
-        // Set csrf at ajax header
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    </script>
+            // Set csrf at ajax header
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        </script>
 
-    <!-- Load global js -->
-    @include('frontend.layouts.global-scripts')
+        <!-- Load global js -->
+        @include('frontend.layouts.global-scripts')
 
-    @stack('scripts')
+        @stack('scripts')
 </body>
 
 </html>

229f31096148c33718186d18edc49099a12929c6
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-08 11:45:58 +0600

    Add to Cart - Showing Success Message And Handling Errors

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -13,11 +13,10 @@ class CartController extends Controller
      *  Add product in to cart
      */
     function addToCart(Request $request) {
-        // dd($request->all());
+        try {
         $product = Product::with(['productSizes', 'productOptions'])->findOrFail($request->product_id);
         $productSize = $product->productSizes->where('id', $request->product_size)->first();
         $productOptions = $product->productOptions->whereIn('id', $request->product_option);
-
 
         $options = [
             'product_size' => [],
@@ -54,6 +53,10 @@ class CartController extends Controller
         ]);
 
         return response(['status' => 'success', 'message' => 'Product added into cart!'], 200);
+
+    }catch(\Exception $e){
+        return response(['status' => 'error', 'message' => 'Something went wrong!'], 500);
+    }
 
     }
 }
--- resources/views/frontend/layouts/ajax-files/product-popup-modal.blade.php
+++ resources/views/frontend/layouts/ajax-files/product-popup-modal.blade.php
@@ -157,10 +157,11 @@
                 url: '{{ route("add-to-cart") }}',
                 data: formData,
                 success: function(response){
-
+                    toastr.success(response.message);
                 },
                 error: function(xhr, status, error){
-                    console.error(error);
+                    let errorMessage = xhr.responseJSON.message;
+                    toastr.error(errorMessage);
                 }
             })
         })

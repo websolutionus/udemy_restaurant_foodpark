2a78ee54b1a352e769d5ac529a7e7b83e9d2eac2
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-08 13:36:04 +0600

    Sidebar Cart - Showing Dynamic Cart Products at Sidebar

--- resources/views/frontend/layouts/menu.blade.php
+++ resources/views/frontend/layouts/menu.blade.php
@@ -80,7 +80,25 @@
             <span class="close_cart"><i class="fal fa-times"></i></span>
         </div>
         <ul>
+            @foreach (Cart::content() as $cartProduct)
             <li>
+                <div class="menu_cart_img">
+                    <img src="{{ asset($cartProduct->options->product_info['image']) }}" alt="menu" class="img-fluid w-100">
+                </div>
+                <div class="menu_cart_text">
+                    <a class="title" href="{{ route('product.show', $cartProduct->options->product_info['slug']) }}">{!! $cartProduct->name !!} </a>
+                    <p class="size">Qty: {{ $cartProduct->qty }}</p>
+
+                    <p class="size">{{ @$cartProduct->options->product_size['name'] }}</p>
+                    @foreach ($cartProduct->options->product_options as $cartProductOption)
+                    <span class="extra">{{ $cartProductOption['name'] }}</span>
+                    @endforeach
+                    <p class="price">{{ currencyPosition($cartProduct->price) }}</p>
+                </div>
+                <span class="del_icon"><i class="fal fa-times"></i></span>
+            </li>
+            @endforeach
+            {{-- <li>
                 <div class="menu_cart_img">
                     <img src="images/menu8.png" alt="menu" class="img-fluid w-100">
                 </div>
@@ -92,57 +110,8 @@
                     <p class="price">$99.00 <del>$110.00</del></p>
                 </div>
                 <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
-            <li>
-                <div class="menu_cart_img">
-                    <img src="images/menu4.png" alt="menu" class="img-fluid w-100">
-                </div>
-                <div class="menu_cart_text">
-                    <a class="title" href="#">Chicken Masalas</a>
-                    <p class="size">medium</p>
-                    <span class="extra">7up</span>
-                    <p class="price">$70.00</p>
-                </div>
-                <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
-            <li>
-                <div class="menu_cart_img">
-                    <img src="images/menu5.png" alt="menu" class="img-fluid w-100">
-                </div>
-                <div class="menu_cart_text">
-                    <a class="title" href="#">Competently Supply Customized Initiatives</a>
-                    <p class="size">large</p>
-                    <span class="extra">coca-cola</span>
-                    <span class="extra">7up</span>
-                    <p class="price">$120.00 <del>$150.00</del></p>
-                </div>
-                <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
-            <li>
-                <div class="menu_cart_img">
-                    <img src="images/menu6.png" alt="menu" class="img-fluid w-100">
-                </div>
-                <div class="menu_cart_text">
-                    <a class="title" href="#">Hyderabadi Biryani</a>
-                    <p class="size">small</p>
-                    <span class="extra">7up</span>
-                    <p class="price">$59.00</p>
-                </div>
-                <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
-            <li>
-                <div class="menu_cart_img">
-                    <img src="images/menu1.png" alt="menu" class="img-fluid w-100">
-                </div>
-                <div class="menu_cart_text">
-                    <a class="title" href="#">Competently Supply</a>
-                    <p class="size">medium</p>
-                    <span class="extra">coca-cola</span>
-                    <span class="extra">7up</span>
-                    <p class="price">$99.00 <del>$110.00</del></p>
-                </div>
-                <span class="del_icon"><i class="fal fa-times"></i></span>
-            </li>
+            </li> --}}
+
         </ul>
         <p class="subtotal">sub total <span>$1220.00</span></p>
         <a class="cart_view" href="cart_view.html"> view cart</a>

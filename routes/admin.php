8b975c78d9256b798673356ee294560a9bdf8805
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-12 12:42:46 +0600

    Coupon Feature - Creating Necessary Files and Designs

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CouponController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\ProductGalleryController;
 use App\Http\Controllers\Admin\ProductOptionController;
@@ -45,6 +46,9 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
 
     /** Product Size Routes */
     Route::resource('product-option', ProductOptionController::class);
+
+    /** Coupon Routes */
+    Route::resource('coupon', CouponController::class);
 
     /** Setting Routes */
     Route::get('/setting', [SettingController::class, 'index'])->name('setting.index');

d54b3431643bc8c0ebfbde7e1dca6657e061356c
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-16 10:56:09 +0600

    Customer Address - Working with Update Feature (Part - 3)

--- resources/views/frontend/dashboard/sections/address-section.blade.php
+++ resources/views/frontend/dashboard/sections/address-section.blade.php
@@ -123,7 +123,7 @@
                                 <select id="select_js4" name="area">
                                     <option value="">Slelect Area</option>
                                     @foreach ($deliveryAreas as $area)
-                                        <option value="{{ $area->id }}">{{ $area->area_name }}</option>
+                                        <option @selected($address->delivery_area_id === $area->id) value="{{ $area->id }}">{{ $area->area_name }}</option>
                                     @endforeach
 
                                 </select>
@@ -132,42 +132,42 @@
 
                         <div class="col-md-6 col-lg-12 col-xl-6">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="First Name" name="first_name">
+                                <input type="text" placeholder="First Name" name="first_name" value="{{ $address->first_name }}">
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-12 col-xl-6">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="Last Name" name="last_name">
+                                <input type="text" placeholder="Last Name" name="last_name" value="{{ $address->last_name }}">
                             </div>
                         </div>
 
                         <div class="col-md-6 col-lg-12 col-xl-6">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="Phone" name="phone">
+                                <input type="text" placeholder="Phone" name="phone" value="{{ $address->phone }}">
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-12 col-xl-6">
                             <div class="fp__check_single_form">
-                                <input type="text" placeholder="Email" name="email">
+                                <input type="text" placeholder="Email" name="email" value="{{ $address->email }}">
                             </div>
                         </div>
 
                         <div class="col-md-12 col-lg-12 col-xl-12">
                             <div class="fp__check_single_form">
-                                <textarea cols="3" rows="4" placeholder="Address" name="address"></textarea>
+                                <textarea cols="3" rows="4" placeholder="Address" name="address">{!! $address->address !!}</textarea>
                             </div>
                         </div>
                         <div class="col-12">
                             <div class="fp__check_single_form check_area">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="type"
+                                    <input @checked($address->type === 'home') class="form-check-input" type="radio" name="type"
                                         id="flexRadioDefault1" value="home">
                                     <label class="form-check-label" for="flexRadioDefault1">
                                         home
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="type"
+                                    <input @checked($address->type === 'office') class="form-check-input" type="radio" name="type"
                                         id="flexRadioDefault2" value="office">
                                     <label class="form-check-label" for="flexRadioDefault2">
                                         office
@@ -193,9 +193,16 @@
         $(document).ready(function(){
             $('.show_edit_section').on('click', function(){
                 let className = $(this).data('class');
-                $('.fp_dashboard_edit_address').removeClass('d-block')
+                $('.fp_dashboard_edit_address').removeClass('d-block');
+                $('.fp_dashboard_edit_address').removeClass('d-none');
+
                 $('.fp_dashboard_existing_address').addClass('d-none');
                 $('.'+className).addClass('d-block');
+            })
+
+            $('.cancel_edit_address').on('click', function(){
+                $('.fp_dashboard_edit_address').addClass('d-none');
+                $('.fp_dashboard_existing_address').removeClass('d-none');
             })
         })
     </script>

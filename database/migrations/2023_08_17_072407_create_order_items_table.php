1c04018bbf5101723dcf78d98d25cfd163146052
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-17 14:02:07 +0600

    Order Placing - Creating Model Migration and Columns for Order (Part -2)

--- database/migrations/2023_08_17_072407_create_order_items_table.php
+++ database/migrations/2023_08_17_072407_create_order_items_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->string('product_name');
+            $table->foreignId('product_id');
+            $table->double('unit_price');
+            $table->integer('qty');
+            $table->json('product_size')->nullable();
+            $table->json('product_option')->nullable();
             $table->timestamps();
         });
     }

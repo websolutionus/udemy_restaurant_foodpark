623a5197e10816eabb355310bddbf5286775f311
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-22 11:34:41 +0600

    Razorpay Setting -  Creating Necessary Files and Designs

--- resources/views/admin/payment-setting/index.blade.php
+++ resources/views/admin/payment-setting/index.blade.php
@@ -21,7 +21,7 @@
                           <a class="nav-link" id="profile-tab4" data-toggle="tab" href="#stripe-setting" role="tab" aria-controls="profile" aria-selected="false">Stripe</a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" id="contact-tab4" data-toggle="tab" href="#contact4" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                          <a class="nav-link" id="contact-tab4" data-toggle="tab" href="#razorpay-setting" role="tab" aria-controls="contact" aria-selected="false">Razorpay</a>
                         </li>
                       </ul>
                     </div>
@@ -31,10 +31,9 @@
 
                         @include('admin.payment-setting.sections.stripe')
 
+                        @include('admin.payment-setting.sections.razorpay')
 
-                        <div class="tab-pane fade" id="contact4" role="tabpanel" aria-labelledby="contact-tab4">
-                          Vestibulum imperdiet odio sed neque ultricies, ut dapibus mi maximus. Proin ligula massa, gravida in lacinia efficitur, hendrerit eget mauris. Pellentesque fermentum, sem interdum molestie finibus, nulla diam varius leo, nec varius lectus elit id dolor. Nam malesuada orci non ornare vulputate. Ut ut sollicitudin magna. Vestibulum eget ligula ut ipsum venenatis ultrices. Proin bibendum bibendum augue ut luctus.
-                        </div>
+
                       </div>
                     </div>
                   </div>
---
+++ resources/views/admin/payment-setting/sections/razorpay.blade.php
@@ -0,0 +1,93 @@
+<div class="tab-pane fade show" id="razorpay-setting" role="tabpanel" aria-labelledby="home-tab4">
+    <div class="card">
+        <div class="card-body border">
+            <form action="{{ route('admin.stripe-setting.update') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+
+                <div class="form-group">
+                    <label for="">Razorpay Status</label>
+                    <select name="razorpay_status" id="" class="select3 form-control">
+                        <option @selected(@$paymentGateway['razorpay_status'] === 1) value="1">Active</option>
+                        <option @selected(@$paymentGateway['razorpay_status'] === 0) value="0">Inactive</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="">Razorpay Country Name</label>
+                    <select name="razorpay_country" id="" class="select3 form-control">
+                        <option value="">Select</option>
+                        @foreach (config('country_list') as $key => $country)
+                            <option @selected(@$paymentGateway['razorpay_country'] === $key) value="{{ $key }}">{{ $country }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="">Razorpay Currency</label>
+                    <select name="razorpay_currency" id="" class="select3 form-control">
+                        <option value="">Select</option>
+                        @foreach (config('currencys.currency_list') as $currency)
+                            <option @selected(@$paymentGateway['razorpay_currency'] === $currency) value="{{ $currency }}">{{ $currency }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="">Currency Rate ( Per {{ config('settings.site_default_currency') }} )</label>
+                    <input name="razorpay_rate" type="text" class="form-control"
+                        value="{{ @$paymentGateway['razorpay_rate'] }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Razorpay Key</label>
+                    <input name="razorpay_api_key" type="text" class="form-control"
+                        value="{{ @$paymentGateway['razorpay_api_key'] }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="">Razorpay Secret Key</label>
+                    <input name="razorpay_secret_key" type="text" class="form-control"
+                        value="{{ @$paymentGateway['razorpay_secret_key'] }}">
+                </div>
+
+                <div class="form-group">
+                    <label>Razorpay Logo</label>
+                    <div id="image-preview-3" class="image-preview razorpay-preview">
+                        <label for="image-upload-3" id="image-label-3">Choose File</label>
+                        <input type="file" name="razorpay_logo" id="image-upload-3" />
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
+</div>
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('.razorpay-preview').css({
+                'background-image': 'url({{ @$paymentGateway["razorpay_logo"] }})',
+                'background-size': 'cover',
+                'background-position': 'center center'
+            })
+
+            if (jQuery().select2) {
+                $(".select3").select2();
+            }
+
+            $.uploadPreview({
+                input_field: "#image-upload-3", // Default: .image-upload
+                preview_box: "#image-preview-3", // Default: .image-preview
+                label_field: "#image-label-3", // Default: .image-label
+                label_default: "Choose File", // Default: Choose File
+                label_selected: "Change File", // Default: Change File
+                no_label: false, // Default: false
+                success_callback: null // Default: null
+            });
+        })
+    </script>
+@endpush

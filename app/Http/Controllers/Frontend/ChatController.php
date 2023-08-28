b45873ad9a9adf716f11d7fa3f2f1940c1edc172
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-08-28 16:39:53 +0600

    Real Time Chatting - Working With Message Sending Feature (Part - 2)

--- app/Http/Controllers/Frontend/ChatController.php
+++ app/Http/Controllers/Frontend/ChatController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Chat;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
     function sendMessage(Request $request) {
-        dd($request->all());
+        $request->validate([
+            'message' => ['required', 'max:1000'],
+            'receiver_id' => ['required', 'integer']
+        ]);
+
+        $chat = new Chat();
+        $chat->sender_id = auth()->user()->id;
+        $chat->receiver_id = $request->receiver_id;
+        $chat->message = $request->message;
+        $chat->save();
+
+        return response(['status' => 'success']);
     }
 }
--- resources/views/frontend/dashboard/sections/message-section.blade.php
+++ resources/views/frontend/dashboard/sections/message-section.blade.php
@@ -5,58 +5,8 @@
             <h3>Message</h3>
             <div class="fp__chat_area">
                 <div class="fp__chat_body">
-                    <div class="fp__chating">
-                        <div class="fp__chating_img">
-                            <img src="images/service_provider.png" alt="person"
-                                class="img-fluid w-100">
-                        </div>
-                        <div class="fp__chating_text">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                Pariatur qui amet aperiam, magni accusamus voluptatum
-                                neque
-                                aut tenetur odit officia fugit et sint harum inventore
-                                recusandae id quibusdam ducimus consequuntur.</p>
-                            <span>15 Jun, 2023, 05:26 AM</span>
-                        </div>
-                    </div>
-                    <div class="fp__chating tf_chat_right">
-                        <div class="fp__chating_img">
-                            <img src="images/client_img_1.jpg" alt="person"
-                                class="img-fluid w-100">
-                        </div>
-                        <div class="fp__chating_text">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            </p>
-                            <span>15 Jun, 2023, 05:26 AM</span>
-                        </div>
-                    </div>
-                    <div class="fp__chating">
-                        <div class="fp__chating_img">
-                            <img src="images/service_provider.png" alt="person"
-                                class="img-fluid w-100">
-                        </div>
-                        <div class="fp__chating_text">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                Pariatur qui amet aperiam.</p>
-                            <span>15 Jun, 2023, 05:26 AM</span>
-                        </div>
-                    </div>
-                    <div class="fp__chating tf_chat_right">
-                        <div class="fp__chating_img">
-                            <img src="images/client_img_1.jpg" alt="person"
-                                class="img-fluid w-100">
-                        </div>
-                        <div class="fp__chating_text">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                Pariatur qui amet aperiam, magni accusamus the in
-                                voluptatum
-                                neque
-                                aut tenetur odit officia fugit et sint harum inventore
-                                recusandae id quibusdam.</p>
-                            <span>15 Jun, 2023, 05:26 AM</span>
-                        </div>
-                    </div>
-                    <div class="fp__chating">
+
+                    {{-- <div class="fp__chating">
                         <div class="fp__chating_img">
                             <img src="images/service_provider.png" alt="person"
                                 class="img-fluid w-100">
@@ -66,8 +16,8 @@
                             </p>
                             <span>15 Jun, 2023, 05:26 AM</span>
                         </div>
-                    </div>
-                    <div class="fp__chating tf_chat_right">
+                    </div> --}}
+                    {{-- <div class="fp__chating tf_chat_right">
                         <div class="fp__chating_img">
                             <img src="images/client_img_1.jpg" alt="person"
                                 class="img-fluid w-100">
@@ -77,14 +27,14 @@
                             </p>
                             <span>15 Jun, 2023, 05:26 AM</span>
                         </div>
-                    </div>
+                    </div> --}}
                 </div>
                 <form class="fp__single_chat_bottom chat_input">
                     @csrf
                     <label for="select_file"><i class="far fa-file-medical"
                             aria-hidden="true"></i></label>
                     <input id="select_file" type="file" hidden="">
-                    <input type="text" placeholder="Type a message..." name="message">
+                    <input type="text" placeholder="Type a message..." name="message" class="fp_send_message">
                     <input type="hidden" name="receiver_id" value="1">
 
                     <button class="fp__massage_btn" type="submit"><i class="fas fa-paper-plane"
@@ -105,9 +55,30 @@
                     method: 'POST',
                     url: "{{ route('chat.send-message') }}",
                     data: formData,
+                    beforeSend: function(){
+                    let message = $('.fp_send_message').val();
+                    let html =  `
+                        <div class="fp__chating tf_chat_right">
+                            <div class="fp__chating_img">
+                                <img src="images/client_img_1.jpg" alt="person"
+                                    class="img-fluid w-100">
+                            </div>
+                            <div class="fp__chating_text">
+                                <p>${message}</p>
+                                <span>15 Jun, 2023, 05:26 AM</span>
+                            </div>
+                        </div>`
+
+                        $('.fp__chat_body').append(html)
+
+                    },
                     success: function(response){
                     },
                     error: function(xhr, status, error){
+                        let errors = xhr.responseJSON.errors;
+                        $.each(errors, function(key, value){
+                            toastr.error(value);
+                        })
                     }
                 })
             })

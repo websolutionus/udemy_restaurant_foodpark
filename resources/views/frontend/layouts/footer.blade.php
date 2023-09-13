0ca5755638791001e616c1c7fbb90f7441e5216b
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-09-13 11:03:38 +0600

    Footer Info - Showing Footer Info at Frontend

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,3 +1,7 @@
+@php
+    $footerInfo = \App\Models\FooterInfo::first();
+@endphp
+
 <footer>
     <div class="footer_overlay pt_100 xs_pt_70 pb_100 xs_pb_70">
         <div class="container wow fadeInUp" data-wow-duration="1s">
@@ -7,14 +11,19 @@
                         <a class="footer_logo" href="index.html">
                             <img src="images/footer_logo.png" alt="FoodPark" class="img-fluid w-100">
                         </a>
-                        <span>There are many variations of Lorem Ipsum available, but the majority have
-                            suffered.</span>
-                        <p class="info"><i class="far fa-map-marker-alt"></i> 7232 Broadway Suite 308, Jackson
-                            Heights, 11372, NY, United States</p>
-                        <a class="info" href="callto:1234567890123"><i class="fas fa-phone-alt"></i>
-                            +1347-430-9510</a>
-                        <a class="info" href="mailto:websolutionus1@gmail.com"><i class="fas fa-envelope"></i>
-                            websolutionus1@gmail.com</a>
+                        @if (@$footerInfo->short_info)
+                        <span>{{ @$footerInfo->short_info }}</span>
+                        @endif
+                        @if (@$footerInfo->address)
+                        <p class="info"><i class="far fa-map-marker-alt"></i> {{ @$footerInfo->address }}</p>
+                        @endif
+                        @if (@$footerInfo->phone)
+                        <a class="info" href="callto:{{ @$footerInfo->phone }}"><i class="fas fa-phone-alt"></i> {{ @$footerInfo->phone }}</a>
+                        @endif
+                        @if (@$footerInfo->email)
+                        <a class="info" href="mailto:{{ @$footerInfo->email }}"><i class="fas fa-envelope"></i>
+                            {{ $footerInfo->email }}</a>
+                        @endif
                     </div>
                 </div>
                 <div class="col-lg-2 col-sm-4 col-md-6">
@@ -71,7 +80,9 @@
             <div class="row">
                 <div class="col-12">
                     <div class="fp__footer_bottom_text d-flex flex-wrap justify-content-between">
-                        <p>Copyright 2022 <b>FoodPark</b> All Rights Reserved.</p>
+                        @if (@$footerInfo->copyright)
+                            <p>{{ @$footerInfo->copyright }}</p>
+                        @endif
                         <ul class="d-flex flex-wrap">
                             <li><a href="#">FAQs</a></li>
                             <li><a href="#">payment</a></li>

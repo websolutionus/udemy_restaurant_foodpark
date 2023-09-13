728d6a4791f747aeb5df033ddfe7b19593edb1d0
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-09-13 16:31:38 +0600

    Menu Builder - Creating Seeders for Menu Builder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call(CategorySeeder::class);
         \App\Models\Product::factory(10)->create();
         \App\Models\Coupon::factory(3)->create();
+        $this->call(MenuBuilderSeeder::class);
 
     }
 }

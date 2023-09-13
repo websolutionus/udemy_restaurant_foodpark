32517c619c035ab285d52724717b34684b1c9985
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-09-13 13:17:54 +0600

    Menu Builder - Working with Footer Menu Two and Three

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,6 +1,9 @@
 @php
     $footerInfo = \App\Models\FooterInfo::first();
     $footerMenuOne = Menu::getByName('footer_menu_one');
+    $footerMenuTwo = Menu::getByName('footer_menu_two');
+    $footerMenuThree = Menu::getByName('footer_menu_three');
+
 @endphp
 
 <footer>
@@ -34,7 +37,6 @@
                             @foreach ($footerMenuOne as $menuItem)
                             <li><a href="{{ $menuItem['link'] }}">{{ $menuItem['label'] }}</a></li>
                             @endforeach
-
                         </ul>
                     </div>
                 </div>
@@ -42,11 +44,9 @@
                     <div class="fp__footer_content">
                         <h3>Help Link</h3>
                         <ul>
-                            <li><a href="#">Terms And Conditions</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Refund Policy</a></li>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">contact</a></li>
+                            @foreach ($footerMenuTwo as $menuItem)
+                            <li><a href="{{ $menuItem['link'] }}">{{ $menuItem['label'] }}</a></li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
@@ -84,10 +84,9 @@
                             <p>{{ @$footerInfo->copyright }}</p>
                         @endif
                         <ul class="d-flex flex-wrap">
-                            <li><a href="#">FAQs</a></li>
-                            <li><a href="#">payment</a></li>
-                            <li><a href="#">settings</a></li>
-                            <li><a href="#">privacy policy</a></li>
+                            @foreach ($footerMenuThree as $menuItem)
+                            <li><a href="{{ $menuItem['link'] }}">{{ $menuItem['label'] }}</a></li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>

bb6011312441e18488106c4f71c39492225c1c8a
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-09-17 15:09:16 +0600

    Settings Feature - Working with Logo Favicon Settings (Part - 1)

--- resources/views/admin/setting/index.blade.php
+++ resources/views/admin/setting/index.blade.php
@@ -13,32 +13,40 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-12 col-sm-12 col-md-2">
-                      <ul class="nav nav-pills flex-column" id="myTab4" role="tablist">
-                        <li class="nav-item">
-                          <a class="nav-link active" id="home-tab4" data-toggle="tab" href="#general-setting" role="tab" aria-controls="home" aria-selected="true">General Settings</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" id="profile-tab4" data-toggle="tab" href="#pusher-setting" role="tab" aria-controls="profile" aria-selected="false">Pusher Settings</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" id="" data-toggle="tab" href="#mail-setting" role="tab" aria-controls="contact" aria-selected="false">Mail Settings</a>
-                        </li>
-                      </ul>
+                        <ul class="nav nav-pills flex-column" id="myTab4" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="home-tab4" data-toggle="tab" href="#general-setting"
+                                    role="tab" aria-controls="home" aria-selected="true">General Settings</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="home-tab4" data-toggle="tab" href="#logo-setting" role="tab"
+                                    aria-controls="home" aria-selected="true">Logo Settings</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="profile-tab4" data-toggle="tab" href="#pusher-setting"
+                                    role="tab" aria-controls="profile" aria-selected="false">Pusher Settings</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="" data-toggle="tab" href="#mail-setting" role="tab"
+                                    aria-controls="contact" aria-selected="false">Mail Settings</a>
+                            </li>
+                        </ul>
                     </div>
                     <div class="col-12 col-sm-12 col-md-10">
-                      <div class="tab-content no-padding" id="myTab2Content">
+                        <div class="tab-content no-padding" id="myTab2Content">
 
-                        @include('admin.setting.sections.general-setting')
+                            @include('admin.setting.sections.general-setting')
 
-                        @include('admin.setting.sections.pusher-setting')
+                            @include('admin.setting.sections.logo-setting')
 
-                        @include('admin.setting.sections.mail-setting')
+                            @include('admin.setting.sections.pusher-setting')
 
-                      </div>
+                            @include('admin.setting.sections.mail-setting')
+
+                        </div>
                     </div>
-                  </div>
+                </div>
             </div>
         </div>
     </section>
 @endsection
-
---
+++ resources/views/admin/setting/sections/logo-setting.blade.php
@@ -0,0 +1,92 @@
+<div class="tab-pane fade show" id="logo-setting" role="tabpanel" aria-labelledby="home-tab4">
+    <div class="card">
+        <div class="card-body border">
+            <form action="{{ route('admin.general-setting.update') }}" method="POST">
+                @csrf
+                @method('PUT')
+
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label>Logo</label>
+                            <div id="image-preview" class="image-preview">
+                                <label for="image-upload" id="image-label">Choose File</label>
+                                <input type="file" name="logo" id="image-upload" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label>Footer Logo</label>
+                            <div id="image-preview-2" class="image-preview">
+                                <label for="image-upload-2" id="image-label-2">Choose File</label>
+                                <input type="file" name="footer_logo" id="image-upload-2" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label>Favicon</label>
+                            <div id="image-preview-3" class="image-preview">
+                                <label for="image-upload-3" id="image-label-3">Choose File</label>
+                                <input type="file" name="favicon" id="image-upload-3" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label>Breadcrumb</label>
+                            <div id="image-preview-4" class="image-preview">
+                                <label for="image-upload-4" id="image-label-4">Choose File</label>
+                                <input type="file" name="breadcrumb" id="image-upload-4" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+    <script>
+        $.uploadPreview({
+            input_field: "#image-upload", // Default: .image-upload
+            preview_box: "#image-preview", // Default: .image-preview
+            label_field: "#image-label", // Default: .image-label
+            label_default: "Choose File", // Default: Choose File
+            label_selected: "Change File", // Default: Change File
+            no_label: false, // Default: false
+            success_callback: null // Default: null
+        });
+        $.uploadPreview({
+            input_field: "#image-upload-2", // Default: .image-upload
+            preview_box: "#image-preview-2", // Default: .image-preview
+            label_field: "#image-label-2", // Default: .image-label
+            label_default: "Choose File", // Default: Choose File
+            label_selected: "Change File", // Default: Change File
+            no_label: false, // Default: false
+            success_callback: null // Default: null
+        });
+        $.uploadPreview({
+            input_field: "#image-upload-3", // Default: .image-upload
+            preview_box: "#image-preview-3", // Default: .image-preview
+            label_field: "#image-label-3", // Default: .image-label
+            label_default: "Choose File", // Default: Choose File
+            label_selected: "Change File", // Default: Change File
+            no_label: false, // Default: false
+            success_callback: null // Default: null
+        });
+        $.uploadPreview({
+            input_field: "#image-upload-4", // Default: .image-upload
+            preview_box: "#image-preview-4", // Default: .image-preview
+            label_field: "#image-label-4", // Default: .image-label
+            label_default: "Choose File", // Default: Choose File
+            label_selected: "Change File", // Default: Change File
+            no_label: false, // Default: false
+            success_callback: null // Default: null
+        });
+    </script>
+@endpush
